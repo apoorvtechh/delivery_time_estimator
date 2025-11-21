3fe889a2f1e74a67bd857bece4d2879f24208589
apoorvtechh <apoorvtechh@gmail.com>
2025-11-21 14:00:13 +0530

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End-to-end machine learning pipeline for delivery ETA prediction using DVC, MLflow, and modular data workflows.',
+    author='Apoorv',
+    license='MIT',
+)
